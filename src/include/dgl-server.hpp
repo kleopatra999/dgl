b5638ef65ed9554cb7147e955cb1c336749c162c
Gilbert Röhrbein <payload@payload-bay.de>
2014-07-01 14:23:04 +0200

    dgl-server.hpp: dgl_exec_func from gles2_exec and egl_exec

--- src/include/dgl-server.hpp
+++ src/include/dgl-server.hpp
@@ -1,11 +1,14 @@
 #pragma once
 #include "dgl.hpp"
+#include <iostream>
 #include <SDL2/SDL.h>
 #include <boost/asio.hpp>
 
 using boost::asio::ip::tcp;
 
-typedef void (*ExecFunc)(char *buf);
+typedef void (*exec_func_t)(
+    char *          buf,
+    std::ostream&   reply_stream);
 
 extern const char *_dgl_pushRet_ptr;
 extern uint32_t    _dgl_pushRet_size;
@@ -15,6 +18,19 @@ extern bool        exec_dont_delete_args;
 SDL_Window *        dgl_main_window();
 SDL_Window *        dgl_make_window();
 void                dgl_make_main_window();
-ExecFunc            dgl_exec_func(uint16_t id);
 void                dgl_init_exec_funcs();
 void                dgl_handle_call(tcp::socket& socket);
+
+
+
+exec_func_t         gles2_exec      (uint16_t id);
+exec_func_t         egl_exec        (uint16_t id);
+
+static
+exec_func_t         dgl_exec_func(uint16_t id) {
+    if (id & gles2_partition)
+        return gles2_exec   (id ^ id & gles2_partition);
+    if (id & egl_partition  )
+        return egl_exec     (id ^ id & egl_partition);
+    return nullptr;
+}
