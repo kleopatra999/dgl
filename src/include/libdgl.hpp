bcc74c719cb35985aa7acfe4b22396c4dd087a44
Gilbert Röhrbein <payload@payload-bay.de>
2014-07-01 14:26:07 +0200

    dgl_sync: typedef for return_buffers, removed dgl_sync(void)

--- src/include/libdgl.hpp
+++ src/include/libdgl.hpp
@@ -8,12 +8,12 @@
 #include <boost/asio.hpp>
 
 using namespace std;
-using namespace boost::asio;
+using boost::asio::mutable_buffers_1;
+typedef vector<mutable_buffers_1> buffers;
 
 bool                    dgl_init(string mode = "x11");
 bool                    dgl_is_init();
-void                    dgl_sync(mutable_buffers_1 return_buffer);
-mutable_buffers_1       dgl_sync();
+void                    dgl_sync(buffers return_buffers);
 vector<Instruction>&    dgl_instructions();
 void                    dgl_swap();
 
