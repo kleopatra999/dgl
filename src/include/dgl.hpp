584235371f927cb8d90ea82b886b8f408556e4f4
Gilbert Röhrbein <payload@payload-bay.de>
2014-07-01 14:22:20 +0200

    dgl.hpp: dgl_func_name, from gles2_name and egl_name

--- src/include/dgl.hpp
+++ src/include/dgl.hpp
@@ -1,5 +1,17 @@
 #pragma once
 #include <string>
 
-void                dgl_init_func_names();
-std::string         dgl_func_name(uint16_t id);
+static const uint16_t gles2_partition   = 0x0100;
+static const uint16_t egl_partition     = 0x0200;
+
+std::string         gles2_name      (uint16_t id);
+std::string         egl_name        (uint16_t id);
+
+static
+std::string         dgl_func_name   (uint16_t id) {
+    if (id & gles2_partition)
+        return gles2_name   (id ^ id & gles2_partition);
+    if (id & egl_partition  )
+        return egl_name     (id ^ id & egl_partition);
+    return "unknown";
+}
