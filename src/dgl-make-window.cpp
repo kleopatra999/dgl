61bbe9a2ca13ce835f3b64d45016bcc17deab6a9
Gilbert Röhrbein <payload@payload-bay.de>
2014-07-01 14:21:28 +0200

    dgl-make-window: make it with eglut, added eglut/

--- src/dgl-make-window.cpp
+++ src/dgl-make-window.cpp
@@ -1,59 +1,24 @@
+#include <unistd.h>
 #include <iostream>
-#include <GL/glew.h>
-#include <SDL2/SDL.h>
+#include <EGL/egl.h>
+#include <GLES2/gl2.h>
+#include "eglut/eglut.h"
 
 using namespace std;
 
-static SDL_Window *main_window;
-
-SDL_Window *dgl_main_window() { return main_window; }
-
-static void sdldie(const char *msg) {
-    auto err = SDL_GetError();
-    cerr << "ERROR " << msg << ": " << err << "\n";
-    SDL_Quit();
-    exit(1);
-}
-
-SDL_Window *dgl_make_window() {
-    SDL_Window     *window;
-    SDL_GLContext   context;
-    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
-        sdldie("SDL_Init");
-    }
-    // OpenGL Profile
-    //SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
-    //SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 2);
-
-    SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
-    SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
-
-    window  = SDL_CreateWindow("DGL",
-        SDL_WINDOWPOS_CENTERED,
-        SDL_WINDOWPOS_CENTERED,
-        640, 480,
-        SDL_WINDOW_OPENGL | SDL_WINDOW_SHOWN);
-    if (!window) {
-        sdldie("SDL_CreateWindow");
-    }
-	
-    context = SDL_GL_CreateContext(window);
-
-    SDL_GL_SetSwapInterval(1);
-
-    if (getenv("DGL_RED_WINDOW")) {
-        glClearColor(1.0, 0.0, 0.0, 1.0);
-    }
-    glClear(GL_COLOR_BUFFER_BIT);
-
-    SDL_GL_SwapWindow(window);
-
-	if (GLEW_OK != glewInit()) {
-		cerr << "ERROR glewInit" << endl;
-	}
-    return window;
-}
+EGLDisplay egl_display;
+EGLSurface egl_surface;
 
 void dgl_make_main_window() {
-    main_window = dgl_make_window();
+    eglutInitWindowSize(300, 300);
+    eglutInitAPIMask(EGLUT_OPENGL_ES2_BIT);
+    eglutInit(0, { });
+    eglutCreateWindow("es2gears");
+    if (getenv("DGL_RED_WINDOW")) {
+        // TODO window is not read that fast, wait somehow
+        usleep(16000);
+        glClearColor(1.0, 0.0, 0.0, 1.0);
+        glClear(GL_COLOR_BUFFER_BIT);
+        eglutFlip();
+    }
 }
