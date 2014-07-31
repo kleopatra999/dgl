3f5c7166e0114f4b48799c9f31ca9f2c8fac47f2
Gilbert Röhrbein <gilbert@ifsr.de>
2014-07-31 18:34:29 +0200

    make_unique: arrays

--- src/include/make_unique.hpp
+++ src/include/make_unique.hpp
@@ -6,6 +6,14 @@ namespace std {
     template<typename T, typename... A>
     std::unique_ptr<T>
     make_unique(A&&... x) {
-        return std::unique_ptr<T>(new T(std::forward<A>(x)...));
+        return std::unique_ptr<T>(
+            new T(std::forward<A>(x)...));
+    }
+
+    template<typename T>
+    std::unique_ptr<T>
+    make_unique(std::size_t size) {
+        return std::unique_ptr<T>(
+            new typename std::remove_extent<T>::type[size]());
     }
 }
