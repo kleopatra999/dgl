484aca4d63c9df379237a3a9cf0630347a56d04c
Gilbert Röhrbein <gilbert@ifsr.de>
2014-09-22 12:06:21 +0200

    fixed warnings: utils: removed bit masks. arithmetric is just okay

--- src/include/consts.hpp
+++ src/include/consts.hpp
@@ -3,8 +3,6 @@
 #include <string>
 #include <GL/gl.h>
 
-static uint16_t id_DGLSwapBuffers = 1499;
-
 int getTypeSize(GLenum type);
 int getLightParamSize(GLenum type);
 int getFormatSize(GLenum format);
--- src/utils.cpp
+++ src/utils.cpp
@@ -4,9 +4,9 @@
 
 std::string         dgl_func_name   (uint16_t id) {
     if (id & gles2_partition)
-        return gles2_name   (id ^ id & gles2_partition);
+        return gles2_name   (id - gles2_partition);
     if (id & egl_partition  )
-        return egl_name     (id ^ id & egl_partition);
+        return egl_name     (id - egl_partition);
     return "unknown";
 }
 
@@ -14,8 +14,8 @@ std::string         dgl_func_name   (uint16_t id) {
 
 exec_func_t         dgl_exec_func(uint16_t id) {
     if (id & gles2_partition)
-        return gles2_exec   (id ^ id & gles2_partition);
+        return gles2_exec   (id - gles2_partition);
     if (id & egl_partition  )
-        return egl_exec     (id ^ id & egl_partition);
+        return egl_exec     (id - egl_partition);
     return nullptr;
 }
