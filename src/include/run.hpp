ec33133366475b5830a077faccc7f97455495ff4
Gilbert Röhrbein <gilbert@ifsr.de>
2015-02-03 11:09:42 +0100

    run, comments

--- src/include/run.hpp
+++ src/include/run.hpp
@@ -3,6 +3,14 @@
 #include <string>
 #include <vector>
 
+// runs a render server with a file
+// blocks, as long file is run
 void run_dgl_stream(const std::string& path);
+
+// runs a render server with networking
+// blocks, as long connection lasts
 void run_dgl_server();
+
+// runs a command in a new process
+// blocks, returns when new process exits
 void run_dgl_preloaded(const std::vector<std::string>& command);
